ac3a13dff234a53f7b4ea1574fae79e63e068d7b
Jagadeesh Pydi <jagadeeshpydi@gmail.com>
2026-03-10 17:36:57 +0530

    update

---
+++ controlFlow/DemeritPoints.js
@@ -0,0 +1,18 @@
+function check(speed) {
+    const speedLimit = 70;
+    const kmPerPoint = 5;
+
+    if (speed <= speedLimit) {
+        console.log("Ok");
+    } else {
+        const points = Math.floor((speed - speedLimit) / kmPerPoint);
+
+        if (points >= 12) {
+            console.log("License suspended");
+        } else {
+            console.log("Points:", points);
+        }
+    }
+}
+
+check(90);
--- pratice.js
+++ pratice.js
@@ -1,5 +1,16 @@
-let a=10,b=30;
+const studentMarks=[70,80,90];
 
-[a,b]=[b,a]; //destructuring assignment
-console.log(a);
-console.log(b);
+
+function checkGrade(marks){
+    let sum=0;
+    for(let i=0;i<marks.length;i++){
+        sum+=marks[i];
+    }
+    const grade=sum/marks.length;
+    if(grade>=90){
+        console.log("Grade A");
+    }else if(grade>=80){
+        console.log("Grade B");
+    }
+}
+checkGrade(studentMarks);
